6e35b247a571059a2bbb3e63864cc9abb4f1a90b
Rennis Pinheiro <rennissteam@alu.ufc.br>
2024-10-11 10:05:41 -0300

    Create delete-messages.js

---
+++ src/delete-messages.js
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+import twilio from 'twilio';
+
+// Carregar as variáveis de ambiente
+dotenv.config({ path: './config/.env' });
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(accountSid, authToken);
+
+console.log(accountSid, authToken);
+
+async function deleteAllMessages() {
+  try {
+    const messages = await client.messages.list();
+    for (const message of messages) {
+      console.warn(`Deleting ${message.sid}`);
+      await message.remove(); // Aguarde a remoção da mensagem
+    }
+  } catch (error) {
+    console.error('Erro ao deletar mensagens:', error);
+  }
+}
+
+console.log("Starting program");
+deleteAllMessages()
+  .then(() => console.log("DONE"))
+  .catch((err) => console.error(err));
